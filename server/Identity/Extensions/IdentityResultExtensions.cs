894a20d7f3375e6a17e960fc597cc084a5f8ad85
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-10 15:28:26 +0200

    Add extension method for conversion of identity result to application result

---
+++ server/Identity/Extensions/IdentityResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Application.Common.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Extensions
+{
+	public static class IdentityResultExtensions
+	{
+		public static Result ToApplicationResult(this IdentityResult result) => result.Succeeded
+			? Result.CreateSuccess()
+			: Result.CreateFailure(result.Errors.Select(e => e.Description));
+	}
+}
