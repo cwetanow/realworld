dd1e079470e8bd2d1292922d022ddd3a5b53c87f
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-10 16:22:16 +0200

    Add additional information to user profile

--- server/Domain/Entities/UserProfile.cs
+++ server/Domain/Entities/UserProfile.cs
@@ -4,12 +4,17 @@ namespace Domain.Entities
 {
 	public class UserProfile : Entity
 	{
-		public UserProfile(string userId)
+		public UserProfile(string userId, string email, string username)
 		{
 			UserId = userId;
+			Email = email;
+			Username = username;
 		}
 
 		public string UserId { get; set; }
+
+		public string Email { get; set; }
+		public string Username { get; set; }
 
 		public string Bio { get; }
 		public string Image { get; }
