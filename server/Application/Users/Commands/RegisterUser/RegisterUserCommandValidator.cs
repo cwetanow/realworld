bb78cb0bfd3e53f7a10cd1e0df864154bde150d1
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-10 16:23:40 +0200

    Add validator for command

---
+++ server/Application/Users/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Commands.RegisterUser
+{
+	public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+	{
+		public RegisterUserCommandValidator()
+		{
+			RuleFor(c => c.Email).NotEmpty();
+			RuleFor(c => c.Username).NotEmpty();
+			RuleFor(c => c.Password).NotEmpty();
+		}
+	}
+}
