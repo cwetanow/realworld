2357e2a5a00596cfb71acc1f52d31af536622ffe
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-10 15:36:28 +0200

    Create app db context

---
+++ server/Persistence/ConduitDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Domain.Common;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+	public class ConduitDbContext : DbContext
+	{
+		public ConduitDbContext(DbContextOptions<ConduitDbContext> options)
+			: base(options)
+		{
+
+		}
+
+		public DbSet<UserProfile> UserProfiles { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+			{
+				// Configure primary key
+				if (typeof(Entity).IsAssignableFrom(entityType.ClrType))
+				{
+					modelBuilder.Entity(entityType.ClrType)
+						.HasKey(nameof(Entity.Id));
+				}
+
+				// Disable cascade delete
+				modelBuilder.Model.GetEntityTypes()
+					.SelectMany(t => t.GetForeignKeys())
+					.Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade)
+					.ToList()
+					.ForEach(e => e.DeleteBehavior = DeleteBehavior.Restrict);
+
+				// Configure all EntityTypeConfiguration
+				var typesToRegisterConfigurations = Assembly.GetExecutingAssembly().GetTypes()
+					.Where(t =>
+						t.GetInterfaces().Any(gi => gi.IsGenericType &&
+													gi.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>)))
+					.Select(Activator.CreateInstance)
+					.ToList();
+
+				foreach (var configurationInstance in typesToRegisterConfigurations)
+				{
+					modelBuilder.ApplyConfiguration((dynamic)configurationInstance);
+				}
+			}
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ server/Persistence/Extensions/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence.Extensions
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddPersistence(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction)
+		{
+			services
+				.AddDbContext<ConduitDbContext>(optionsAction)
+				.AddScoped<DbContext>(provider => provider.GetService<ConduitDbContext>());
+
+			return services;
+		}
+	}
+}
