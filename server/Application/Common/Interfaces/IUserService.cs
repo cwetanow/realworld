62a812abdf7100c442c8fd227e3e9180b0470da5
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-10 15:27:31 +0200

    Return task from user service

--- server/Application/Common/Interfaces/IUserService.cs
+++ server/Application/Common/Interfaces/IUserService.cs
@@ -1,9 +1,10 @@
-﻿using Application.Common.Models;
+﻿using System.Threading.Tasks;
+using Application.Common.Models;
 
 namespace Application.Common.Interfaces
 {
 	public interface IUserService
 	{
-		(Result result, string userId) CreateUser(string username, string email, string password);
+		Task<(Result result, string userId)> CreateUser(string username, string email, string password);
 	}
 }
