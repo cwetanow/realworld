cc6cbd1abf7d15585de37984f3c5991c81277d6a
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-10 15:36:18 +0200

    Create entity for user profile

---
+++ server/Domain/Entities/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+	public class UserProfile : Entity
+	{
+		public UserProfile(string userId)
+		{
+			UserId = userId;
+		}
+
+		public string UserId { get; set; }
+
+		public string Bio { get; }
+		public string Image { get; }
+	}
+}
