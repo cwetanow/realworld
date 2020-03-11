5432df22fb85446cef406e688bc5452064f50c5e
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-11 11:19:52 +0200

    Implement jwt token logic

---
+++ server/Identity/TokenService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Identity.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Identity
+{
+	public class TokenService
+	{
+		private readonly AuthConfiguration configuration;
+
+		public TokenService(AuthConfiguration configuration)
+		{
+			this.configuration = configuration;
+		}
+
+		public string CreateToken(string username, string userId, params KeyValuePair<string, string>[] additionalClaims)
+		{
+			var claims = new List<Claim> {
+				new Claim(JwtRegisteredClaimNames.Sub, username),
+				new Claim(JwtRegisteredClaimNames.Jti, userId)
+			};
+
+			foreach (var additionalClaim in additionalClaims)
+			{
+				var claim = new Claim(additionalClaim.Key, additionalClaim.Value);
+				claims.Add(claim);
+			}
+
+			return GenerateToken(claims);
+		}
+
+		public string GenerateToken(IEnumerable<Claim> claims)
+		{
+			var expires = DateTime.UtcNow.AddHours(configuration.ValidHours);
+
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.SecretKey));
+			var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(
+				configuration.Issuer,
+				configuration.Audience,
+				claims,
+				expires: expires,
+				signingCredentials: credentials
+			);
+
+			var tokenString = new JwtSecurityTokenHandler()
+				.WriteToken(token);
+
+			return tokenString;
+		}
+	}
+}
