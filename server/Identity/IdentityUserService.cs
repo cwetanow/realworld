35ee8352641ab3fc88c9441f8eb9a8408772f201
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-11 11:20:05 +0200

    Implement user authentication logic

--- server/Identity/IdentityUserService.cs
+++ server/Identity/IdentityUserService.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Application.Common.Interfaces;
 using Application.Common.Models;
+using Identity.Exceptions;
 using Identity.Extensions;
 using Microsoft.AspNetCore.Identity;
 
@@ -10,10 +10,12 @@ namespace Identity
 	public class IdentityUserService : IUserService
 	{
 		private readonly UserManager<ApplicationUser> userManager;
+		private readonly TokenService tokenService;
 
-		public IdentityUserService(UserManager<ApplicationUser> userManager)
+		public IdentityUserService(UserManager<ApplicationUser> userManager, TokenService tokenService)
 		{
 			this.userManager = userManager;
+			this.tokenService = tokenService;
 		}
 
 		public async Task<(Result result, string userId)> CreateUser(string username, string email, string password)
@@ -23,6 +25,27 @@ namespace Identity
 			var result = await userManager.CreateAsync(user, password);
 
 			return (result.ToApplicationResult(), user.Id);
+		}
+
+		public async Task<string> Authenticate(string email, string password)
+		{
+			var user = await userManager.FindByEmailAsync(email);
+
+			if (user == null)
+			{
+				throw new InvalidCredentialsException();
+			}
+
+			var isPasswordCorrect = await userManager.CheckPasswordAsync(user, password);
+
+			if (!isPasswordCorrect)
+			{
+				throw new InvalidCredentialsException();
+			}
+
+			var token = tokenService.CreateToken(user.UserName, user.Id);
+
+			return token;
 		}
 	}
 }
