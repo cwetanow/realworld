c73352dc4d6759f63ce85b23c0e91a7d25c311f2
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-12 15:00:31 +0200

    Set is following flag

--- server/Application.UnitTests/Profile/Queries/ProfileByUsernameQueryTests.cs
+++ server/Application.UnitTests/Profile/Queries/ProfileByUsernameQueryTests.cs
@@ -2,11 +2,13 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Common.Exceptions;
+using Application.Common.Interfaces;
 using Application.Profiles.Models;
 using Application.Profiles.Queries;
 using Application.UnitTests.Common;
 using Domain.Entities;
 using FluentAssertions;
+using Moq;
 using Xunit;
 
 namespace Application.UnitTests.Profile.Queries
@@ -20,7 +22,9 @@ namespace Application.UnitTests.Profile.Queries
 			// Arrange
 			var query = new ProfileByUsernameQuery { Username = username };
 
-			var sut = new ProfileByUsernameQuery.Handler(context: Context, mapper: Mapper);
+			var currentUserServiceMock = new Mock<ICurrentUserService>();
+
+			var sut = new ProfileByUsernameQuery.Handler(context: Context, mapper: Mapper, currentUserServiceMock.Object);
 
 			// Act
 			var action = new Func<Task<ProfileDto>>(async () => await sut.Handle(query, CancellationToken.None));
@@ -40,7 +44,9 @@ namespace Application.UnitTests.Profile.Queries
 
 			var query = new ProfileByUsernameQuery { Username = username };
 
-			var sut = new ProfileByUsernameQuery.Handler(context: Context, mapper: Mapper);
+			var currentUserServiceMock = new Mock<ICurrentUserService>();
+
+			var sut = new ProfileByUsernameQuery.Handler(context: Context, mapper: Mapper, currentUserServiceMock.Object);
 
 			var expectedResult = Mapper.Map<ProfileDto>(profile);
 
@@ -49,6 +55,59 @@ namespace Application.UnitTests.Profile.Queries
 
 			// Assert
 			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+
+		[Theory]
+		[InlineData("username")]
+		public async Task TestHandle_UserIsAuthenticatedAndIsNotFollowing_ShouldSetDtoFollowingToFalse(string username)
+		{
+			// Arrange
+			var profile = new UserProfile(Guid.NewGuid().ToString(), "email", username);
+			Context.UserProfiles.Add(profile);
+			await Context.SaveChangesAsync();
+
+			var query = new ProfileByUsernameQuery { Username = username };
+
+			var currentUserServiceMock = new Mock<ICurrentUserService>();
+
+			var sut = new ProfileByUsernameQuery.Handler(context: Context, mapper: Mapper, currentUserServiceMock.Object);
+
+			// Act
+			var actualResult = await sut.Handle(query, CancellationToken.None);
+
+			// Assert
+			actualResult.Following.Should().BeFalse();
+		}
+
+		[Theory]
+		[InlineData("username")]
+		public async Task TestHandle_UserIsAuthenticatedAndIsFollowing_ShouldSetDtoFollowingToFalse(string username)
+		{
+			// Arrange
+			var currentUserEmail = "currentUserEmail";
+			var currentUserProfile = new UserProfile(Guid.NewGuid().ToString(), currentUserEmail, "currentUsername");
+			var profile = new UserProfile(Guid.NewGuid().ToString(), "email", username);
+
+			Context.UserProfiles.Add(currentUserProfile);
+			Context.UserProfiles.Add(profile);
+			await Context.SaveChangesAsync();
+
+			Context.UserFollowers.Add(new UserFollower(profile.Id, currentUserProfile.Id));
+			await Context.SaveChangesAsync();
+
+			var query = new ProfileByUsernameQuery { Username = username };
+
+			var currentUserServiceMock = new Mock<ICurrentUserService>();
+			currentUserServiceMock.Setup(s => s.IsAuthenticated).Returns(true);
+			currentUserServiceMock.Setup(s => s.Email).Returns(currentUserEmail);
+
+			var sut = new ProfileByUsernameQuery.Handler(context: Context, mapper: Mapper, currentUserServiceMock.Object);
+
+			// Act
+			var actualResult = await sut.Handle(query, CancellationToken.None);
+
+			// Assert
+			actualResult.Following.Should().BeTrue();
 		}
 	}
 }
--- server/Application/Profiles/Models/ProfileDto.cs
+++ server/Application/Profiles/Models/ProfileDto.cs
@@ -9,5 +9,6 @@ namespace Application.Profiles.Models
 		public string Username { get; set; }
 		public string Bio { get; set; }
 		public string Image { get; set; }
+		public bool Following { get; set; }
 	}
 }
