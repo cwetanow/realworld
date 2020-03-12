a11c735e8bc482278d14941e949b0f8807155ea0
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-12 15:13:43 +0200

    Implement unfollow command

--- server/API/Controllers/ProfileController.cs
+++ server/API/Controllers/ProfileController.cs
@@ -11,19 +11,25 @@ namespace API.Controllers
 	public class ProfileController : BaseController
 	{
 		public ProfileController(IMediator mediator) : base(mediator)
-		{
-		}
-
-		[HttpPost("{username}/follow")]
-		public async Task<ProfileDto> FollowUser(string username)
-		{
-			await mediator.Send(new FollowUserCommand { Username = username });
-
-			return await mediator.Send(new ProfileByUsernameQuery { Username = username });
-		}
+		{ }
 
 		[AllowAnonymous]
 		[HttpGet("{username}")]
 		public async Task<ProfileDto> GetProfile(string username) => await mediator.Send(new ProfileByUsernameQuery { Username = username });
+
+		[HttpPost("{username}/follow")]
+		public async Task<ProfileDto> FollowUser(string username) =>
+			await ChangeUserFollowing(new FollowUserCommand { Username = username }, username);
+
+		[HttpDelete("{username}/follow")]
+		public async Task<ProfileDto> UnfollowUser(string username) =>
+			await ChangeUserFollowing(new UnfollowUserCommand { Username = username }, username);
+
+		private async Task<ProfileDto> ChangeUserFollowing(IRequest command, string username)
+		{
+			await mediator.Send(command);
+
+			return await mediator.Send(new ProfileByUsernameQuery { Username = username });
+		}
 	}
 }
---
+++ server/Application/Profiles/Commands/UnfollowUserCommand.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Profiles.Commands
+{
+	public class UnfollowUserCommand : IRequest
+	{
+		public string Username { get; set; }
+
+		public class Handler : IRequestHandler<UnfollowUserCommand>
+		{
+			private readonly ICurrentUserService currentUser;
+			private readonly DbContext context;
+
+			public Handler(ICurrentUserService currentUser, DbContext context)
+			{
+				this.currentUser = currentUser;
+				this.context = context;
+			}
+
+			public async Task<Unit> Handle(UnfollowUserCommand request, CancellationToken cancellationToken)
+			{
+				var currentUserEmail = this.currentUser.Email;
+
+				var currentUserId = await context.Set<UserProfile>()
+					.AsNoTracking()
+					.Where(up => up.Email == currentUserEmail)
+					.Select(u => u.Id)
+					.SingleAsync(cancellationToken);
+
+				var otherUserId = await context.Set<UserProfile>()
+					.AsNoTracking()
+					.Where(up => up.Username == request.Username)
+					.Select(u => u.Id)
+					.SingleOrDefaultAsync(cancellationToken);
+
+				if (otherUserId is default(int))
+				{
+					throw new EntityNotFoundException<UserProfile>(request.Username);
+				}
+
+				var following = await context.Set<UserFollower>()
+					.SingleOrDefaultAsync(f => f.UserId == otherUserId && f.FollowerId == currentUserId, cancellationToken);
+
+				if (following == null)
+				{
+					throw new EntityNotFoundException<UserFollower>(new { UserId = otherUserId, FollowerId = currentUserId });
+				}
+
+				context.Remove(following);
+				await context.SaveChangesAsync(cancellationToken);
+
+				return Unit.Value;
+			}
+		}
+
+		public class Validator : AbstractValidator<UnfollowUserCommand>
+		{
+			public Validator()
+			{
+				RuleFor(c => c.Username).NotEmpty();
+			}
+		}
+	}
+}
