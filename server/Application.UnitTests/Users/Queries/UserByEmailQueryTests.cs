65983505eba943013ec6a49c7e70f042e1302888
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-12 10:58:41 +0200

    Add user by email query with tests

---
+++ server/Application.UnitTests/Users/Queries/UserByEmailQueryTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.UnitTests.Common;
+using Application.Users.Models;
+using Application.Users.Queries;
+using Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Application.UnitTests.Users.Queries
+{
+	public class UserByEmailQueryTests : BaseTestFixture
+	{
+		[Theory]
+		[InlineData("email")]
+		public void TestHandle_EntityDoesNotExist_ShouldThrowEntityNotFoundExceptionWithCorrectErrors(string email)
+		{
+			// Arrange
+			var command = new UserByEmailQuery {
+				Email = email
+			};
+
+			var sut = new UserByEmailQuery.Handler(Context, Mapper);
+
+			// Act
+			var action = new Func<Task<UserWithProfileDto>>(async () => await sut.Handle(command, CancellationToken.None));
+
+			// Assert
+			action.Should().Throw<EntityNotFoundException<UserProfile>>().And.Message.Should().Contain(email);
+		}
+
+		[Theory]
+		[InlineData("email")]
+		public async Task TestHandle_EntityExists_ShouldReturnCorrectly(string email)
+		{
+			// Arrange
+			var userProfile = new UserProfile(Guid.NewGuid().ToString(), email, "username");
+			Context.UserProfiles.Add(userProfile);
+			await Context.SaveChangesAsync();
+
+			var command = new UserByEmailQuery {
+				Email = email
+			};
+
+			var sut = new UserByEmailQuery.Handler(Context, Mapper);
+
+			var expectedResult = Mapper.Map<UserWithProfileDto>(userProfile);
+
+			// Act
+			var actualResult = await sut.Handle(command, CancellationToken.None);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
---
+++ server/Application/Users/Queries/UserByEmailQuery.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Users.Models;
+using AutoMapper;
+using Domain.Entities;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Users.Queries
+{
+	public class UserByEmailQuery : IRequest<UserWithProfileDto>
+	{
+		public string Email { get; set; }
+
+		public class Handler : IRequestHandler<UserByEmailQuery, UserWithProfileDto>
+		{
+			private readonly DbContext context;
+			private readonly IMapper mapper;
+
+			public Handler(DbContext context, IMapper mapper)
+			{
+				this.context = context;
+				this.mapper = mapper;
+			}
+
+			public async Task<UserWithProfileDto> Handle(UserByEmailQuery request, CancellationToken cancellationToken)
+			{
+				var userProfile = await context.Set<UserProfile>()
+					.AsNoTracking()
+					.SingleOrDefaultAsync(p => p.Email == request.Email, cancellationToken);
+
+				if (userProfile == null)
+				{
+					throw new EntityNotFoundException<UserProfile>(request.Email);
+				}
+
+				return mapper.Map<UserWithProfileDto>(userProfile);
+			}
+		}
+
+		public class Validator : AbstractValidator<UserByEmailQuery>
+		{
+			public Validator()
+			{
+				RuleFor(q => q.Email).NotEmpty();
+			}
+		}
+	}
+}
