4f66abe7c9eb132f792eaf1b2fd852587dce3f3f
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-13 14:37:11 +0200

    Add predicate builder helper extensions

---
+++ server/Common/Helpers/PredicateBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Common.Helpers
+{
+	public static class PredicateBuilder
+	{
+		public static Expression<Func<T, bool>> True<T>() => f => true;
+
+		public static Expression<Func<T, bool>> False<T>() => f => false;
+
+		public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> currentExpression, Expression<Func<T, bool>> otherExpression) =>
+			Expression.Lambda<Func<T, bool>>(Expression.OrElse(currentExpression.Body, Expression.Invoke(otherExpression, currentExpression.Parameters)), currentExpression.Parameters);
+
+		public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> currentExpression, Expression<Func<T, bool>> otherExpression) =>
+			Expression.Lambda<Func<T, bool>>(Expression.AndAlso(currentExpression.Body, Expression.Invoke(otherExpression, currentExpression.Parameters)), currentExpression.Parameters);
+	}
+}
