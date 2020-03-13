690c34b1d50f3552c99ea50f861ff623d7db3423
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-13 14:37:23 +0200

    Implement list articles query

---
+++ server/Application/Articles/Models/ArticleListDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Application.Common.Mappings;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Articles.Models
+{
+	public class ArticleListDto : IMapFrom<IEnumerable<Article>>
+	{
+		public IEnumerable<ArticleDto> Articles { get; set; } = new List<ArticleDto>();
+
+		public int ArticlesCount => Articles.Count();
+
+		public void Mapping(Profile profile) => profile.CreateMap<IEnumerable<ArticleDto>, ArticleListDto>()
+			.ForMember(d => d.Articles, opt => opt.MapFrom(s => s));
+	}
+}
---
+++ server/Application/Articles/Queries/ListArticlesQuery.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Articles.Models;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Common.Helpers;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Articles.Queries
+{
+	public class ListArticlesQuery : IRequest<ArticleListDto>
+	{
+		public string Tag { get; set; }
+		public string Author { get; set; }
+		public string Favorited { get; set; }
+
+		public int Limit { get; set; } = 20;
+		public int Offset { get; set; } = 0;
+
+		public class Handler : IRequestHandler<ListArticlesQuery, ArticleListDto>
+		{
+			private readonly DbContext context;
+			private readonly IMapper mapper;
+
+			public Handler(DbContext context, IMapper mapper)
+			{
+				this.context = context;
+				this.mapper = mapper;
+			}
+
+			public async Task<ArticleListDto> Handle(ListArticlesQuery request, CancellationToken cancellationToken)
+			{
+				var whereExpression = PredicateBuilder.True<Article>();
+
+				if (!string.IsNullOrEmpty(request.Author))
+				{
+					whereExpression = whereExpression
+						.And(a => a.Author.Username == request.Author);
+				}
+
+				if (!string.IsNullOrEmpty(request.Tag))
+				{
+					whereExpression = whereExpression
+						.And(a => a.Tags.Any(t => t.Tag.Value == request.Tag));
+				}
+
+				//if (!string.IsNullOrEmpty(request.Favorited))
+				//{
+				//	whereExpression = whereExpression
+				//		.And(a => a.Author.Username == request.Author);
+				//}
+
+				var articles = await context.Set<Article>()
+					.Where(whereExpression)
+					.ProjectTo<ArticleDto>(mapper.ConfigurationProvider)
+					.OrderByDescending(a => a.UpdatedAt)
+					.Skip(request.Offset)
+					.Take(request.Limit)
+					.ToListAsync(cancellationToken);
+
+				return mapper.Map<ArticleListDto>(articles);
+			}
+		}
+	}
+}
