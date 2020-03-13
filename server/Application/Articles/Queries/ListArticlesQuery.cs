20e9b73a45470dfab274830480c124e83904b051
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-13 14:40:51 +0200

    Add logic for following flag

--- server/Application/Articles/Queries/ListArticlesQuery.cs
+++ server/Application/Articles/Queries/ListArticlesQuery.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Articles.Models;
+using Application.Common.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Common.Helpers;
@@ -24,11 +25,13 @@ namespace Application.Articles.Queries
 		{
 			private readonly DbContext context;
 			private readonly IMapper mapper;
+			private readonly ICurrentUserService currentUser;
 
-			public Handler(DbContext context, IMapper mapper)
+			public Handler(DbContext context, IMapper mapper, ICurrentUserService currentUser)
 			{
 				this.context = context;
 				this.mapper = mapper;
+				this.currentUser = currentUser;
 			}
 
 			public async Task<ArticleListDto> Handle(ListArticlesQuery request, CancellationToken cancellationToken)
@@ -60,6 +63,23 @@ namespace Application.Articles.Queries
 					.Skip(request.Offset)
 					.Take(request.Limit)
 					.ToListAsync(cancellationToken);
+
+				if (currentUser.IsAuthenticated)
+				{
+					var currentUserFollowedAuthors = context.Set<UserFollower>()
+						.Where(f => f.Follower.Email == currentUser.Email)
+						.Select(f => f.User.Username)
+						.Distinct()
+						.ToHashSet();
+
+					foreach (var article in articles)
+					{
+						if (currentUserFollowedAuthors.Contains(article.Author.Username))
+						{
+							article.Author.Following = true;
+						}
+					}
+				}
 
 				return mapper.Map<ArticleListDto>(articles);
 			}
