d0c1ab1331c7b5fa91967784b1f67dc123652142
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-07-13 14:25:09 +0300

    Add controller action

--- server/API/Controllers/ArticlesController.cs
+++ server/API/Controllers/ArticlesController.cs
@@ -9,53 +9,57 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class ArticlesController : BaseController
-    {
-        public ArticlesController(IMediator mediator) : base(mediator)
-        {
-        }
+	public class ArticlesController : BaseController
+	{
+		public ArticlesController(IMediator mediator) : base(mediator)
+		{
+		}
 
-        [HttpPost]
-        public async Task<ArticleDto> CreateArticle([FromBody] ArticleRequest<CreateArticleCommand> request)
-        {
-            var id = await mediator.Send(request.Article);
+		[HttpPost]
+		public async Task<ArticleDto> CreateArticle([FromBody] ArticleRequest<CreateArticleCommand> request)
+		{
+			var id = await mediator.Send(request.Article);
 
-            return await mediator.Send(new ArticleByIdQuery {Id = id});
-        }
+			return await mediator.Send(new ArticleByIdQuery { Id = id });
+		}
 
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<ArticleListDto> ListArticles([FromQuery] ListArticlesQuery query) =>
-            await mediator.Send(query);
+		[HttpGet]
+		[AllowAnonymous]
+		public async Task<ArticleListDto> ListArticles([FromQuery] ListArticlesQuery query) =>
+			await mediator.Send(query);
 
-        [HttpGet("feed")]
-        public async Task<ArticleListDto> FeedArticles([FromQuery] FeedArticlesQuery query) =>
-            await mediator.Send(query);
+		[HttpGet("feed")]
+		public async Task<ArticleListDto> FeedArticles([FromQuery] FeedArticlesQuery query) =>
+			await mediator.Send(query);
 
-        [HttpGet("{slug}")]
-        [AllowAnonymous]
-        public async Task<ArticleDto> GetArticle(string slug) =>
-            await mediator.Send(new ArticleBySlugQuery {Slug = slug});
+		[HttpGet("{slug}")]
+		[AllowAnonymous]
+		public async Task<ArticleDto> GetArticle(string slug) =>
+			await mediator.Send(new ArticleBySlugQuery { Slug = slug });
 
-        [HttpPut("{slug}")]
-        public async Task<ArticleDto> UpdateArticle(string slug, ArticleRequest<UpdateArticleCommand> request)
-        {
-            var command = request.Article;
-            command.Slug = slug;
+		[HttpPut("{slug}")]
+		public async Task<ArticleDto> UpdateArticle(string slug, ArticleRequest<UpdateArticleCommand> request)
+		{
+			var command = request.Article;
+			command.Slug = slug;
 
-            await mediator.Send(command);
+			await mediator.Send(command);
 
-            return await mediator.Send(new ArticleBySlugQuery {Slug = slug});
-        }
+			return await mediator.Send(new ArticleBySlugQuery { Slug = slug });
+		}
 
-        [HttpPost("{slug}/favorite")]
-        public async Task<ArticleDto> FavouriteArticle(string slug)
-        {
-            var command = new FavouriteArticleCommand {Slug = slug};
+		[HttpPost("{slug}/favorite")]
+		public async Task<ArticleDto> FavouriteArticle(string slug)
+		{
+			var command = new FavouriteArticleCommand { Slug = slug };
 
-            await mediator.Send(command);
+			await mediator.Send(command);
 
-            return await mediator.Send(new ArticleBySlugQuery {Slug = slug});
-        }
-    }
+			return await mediator.Send(new ArticleBySlugQuery { Slug = slug });
+		}
+
+		[HttpDelete("{slug}")]
+		public Task Delete(string slug) =>
+			mediator.Send(new DeleteArticleCommand { Slug = slug });
+	}
 }
