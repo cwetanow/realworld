f074232ada9095cbb73a01c1c51f87a12669e9b2
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-08-03 11:25:06 +0300

    FeedArticlesQuery

--- server/Application/Articles/Queries/FeedArticlesQuery.cs
+++ server/Application/Articles/Queries/FeedArticlesQuery.cs
@@ -11,41 +11,41 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Articles.Queries
 {
-    public class FeedArticlesQuery : IRequest<ArticleListDto>
-    {
-        public int Limit { get; set; } = 20;
-        public int Offset { get; set; } = 0;
+	public class FeedArticlesQuery : IRequest<ArticleListDto>
+	{
+		public int Limit { get; set; } = 20;
+		public int Offset { get; set; } = 0;
 
-        public class Handler : IRequestHandler<FeedArticlesQuery, ArticleListDto>
-        {
-            private readonly DbContext context;
-            private readonly IMapper mapper;
-            private readonly ICurrentUserService currentUser;
+		public class Handler : IRequestHandler<FeedArticlesQuery, ArticleListDto>
+		{
+			private readonly DbContext context;
+			private readonly IMapper mapper;
+			private readonly ICurrentUserService currentUser;
 
-            public Handler(DbContext context, IMapper mapper, ICurrentUserService currentUser)
-            {
-                this.context = context;
-                this.mapper = mapper;
-                this.currentUser = currentUser;
-            }
+			public Handler(DbContext context, IMapper mapper, ICurrentUserService currentUser)
+			{
+				this.context = context;
+				this.mapper = mapper;
+				this.currentUser = currentUser;
+			}
 
-            public async Task<ArticleListDto> Handle(FeedArticlesQuery request, CancellationToken cancellationToken)
-            {
-                var articles = await context.Set<Article>()
-                    .Where(a => a.Author.Followers.Any(f => f.Follower.Email == currentUser.Email))
-                    .OrderByDescending(a => a.UpdatedAt)
-                    .Skip(request.Offset)
-                    .Take(request.Limit)
-                    .ProjectTo<ArticleDto>(mapper.ConfigurationProvider)
-                    .ToListAsync(cancellationToken);
+			public async Task<ArticleListDto> Handle(FeedArticlesQuery request, CancellationToken cancellationToken)
+			{
+				var articles = await context.Set<Article>()
+					.Where(a => a.Author.Followers.Any(f => f.FollowerId == currentUser.UserId))
+					.OrderByDescending(a => a.UpdatedAt)
+					.Skip(request.Offset)
+					.Take(request.Limit)
+					.ProjectTo<ArticleDto>(mapper.ConfigurationProvider)
+					.ToListAsync(cancellationToken);
 
-                foreach (var article in articles)
-                {
-                    article.Author.Following = true;
-                }
+				foreach (var article in articles)
+				{
+					article.Author.Following = true;
+				}
 
-                return mapper.Map<ArticleListDto>(articles);
-            }
-        }
-    }
+				return mapper.Map<ArticleListDto>(articles);
+			}
+		}
+	}
 }
